b845dc5fac89d6d6f21b54f9519f66f58cb25bcc
Jerónimo García Enciso <jeronimo.garcia.ext@diagroup.com>
2023-09-05 11:12:12 +0200

    rename to Fragment1

--- app/src/main/java/dev/ronronea/viewpagertabfragmentgestionboilerplate/ui/fragment1/Fragment1.kt
+++ app/src/main/java/dev/ronronea/viewpagertabfragmentgestionboilerplate/ui/fragment1/Fragment1.kt
@@ -1,4 +1,4 @@
-package dev.ronronea.viewpagertabfragmentgestionboilerplate.ui.dashboard
+package dev.ronronea.viewpagertabfragmentgestionboilerplate.ui.fragment1
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import dev.ronronea.viewpagertabfragmentgestionboilerplate.databinding.FragmentDashboardBinding
+import dev.ronronea.viewpagertabfragmentgestionboilerplate.databinding.Fragment1Binding
 
-class DashboardFragment : Fragment() {
+class Fragment1 : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: Fragment1Binding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -23,12 +23,12 @@ class DashboardFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+            ViewModelProvider(this).get(Fragment1ViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = Fragment1Binding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
+        val textView: TextView = binding.text
         dashboardViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
